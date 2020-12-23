b9ea1ddd31bc69e861ed62f97c4ae55438a6eb31
Dhiraj Govindvira <dhirajgovindvira1112@gmail.com>
2020-12-23 16:23:53 +0530

    autosave.

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  title:string = "</Decoder>";
+  title: string = "</Decoder>";
   constructor() { }
 
   ngOnInit(): void {
