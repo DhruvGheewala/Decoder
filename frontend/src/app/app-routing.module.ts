b929ec119f78ab65d73b2e2f5754f0b42fa5327a
Dhiraj Govindvira <dhirajgovindvira1112@gmail.com>
2020-12-24 18:31:22 +0530

    Revert "Merge branch 'main' into DhruvGheewala"
    This reverts commit 0ec5809cff5db82baf41226b3222c10709f4e08a, reversing
    changes made to de515f92d287ed2f5003cbec5584c1ddf4befa2c.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
-import { registerLocaleData } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './components/about/about.component';
-import { BlogCreateComponent } from './components/blog/blog-create/blog-create.component';
-import { BlogEditComponent } from './components/blog/blog-edit/blog-edit.component';
-import { BlogListComponent } from './components/blog/blog-list/blog-list.component';
-import { BlogViewComponent } from './components/blog/blog-view/blog-view.component';
-import { ContactComponent } from './components/contact/contact.component';
 import { ErrorComponent } from './components/error/error.component';
 import { HomeComponent } from './components/home/home.component';
-import { IdeComponent } from './components/ide/ide.component';
-import { LoginComponent } from './components/login/login.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { RegisterComponent } from './components/register/register.component';
 import { UpcomingContestComponent } from './components/upcoming-contest/upcoming-contest.component';
 
 const routes: Routes = [
@@ -25,65 +14,13 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'ide',
-    component: IdeComponent,
-  },
-  {
-    path: 'ide:id',
-    component: IdeComponent,
-  },
-  {
     path: 'upcoming-contest',
     component: UpcomingContestComponent,
-  },
-  {
-    path: 'blog/list',
-    component: BlogListComponent,
-  },
-  {
-    path: 'blog/create',
-    component: BlogCreateComponent,
-  },
-  {
-    path: 'blog/view',
-    component: BlogViewComponent,
-  },
-  {
-    path: 'blog/edit/:id',
-    component: BlogEditComponent,
-  },
-  {
-    path: 'upcoming-contest',
-    component: UpcomingContestComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-  {
-    path: 'profile/:user',
-    component: ProfileComponent,
-  },
-  {
-    path: 'about',
-    component: AboutComponent,
-  },
-  {
-    path: 'contact',
-    component: ContactComponent,
   },
   {
     path: 'error',
     component: ErrorComponent,
   },
-  {
-    path: '**',
-    component: ErrorComponent
-  }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,17 +8,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
 import { UpcomingContestComponent } from './components/upcoming-contest/upcoming-contest.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { LoginComponent } from './components/login/login.component';
-import { BlogViewComponent } from './components/blog/blog-view/blog-view.component';
-import { BlogCreateComponent } from './components/blog/blog-create/blog-create.component';
-import { BlogEditComponent } from './components/blog/blog-edit/blog-edit.component';
-import { BlogListComponent } from './components/blog/blog-list/blog-list.component';
-import { IdeComponent } from './components/ide/ide.component';
+
 import { HttpClientModule } from "@angular/common/http";
-import { RegisterComponent } from './components/register/register.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
@@ -27,17 +18,7 @@ import { AboutComponent } from './components/about/about.component';
     FooterComponent,
     HomeComponent,
     ErrorComponent,
-    UpcomingContestComponent,
-    ContactComponent,
-    LoginComponent,
-    BlogViewComponent,
-    BlogCreateComponent,
-    BlogEditComponent,
-    BlogListComponent,
-    IdeComponent,
-    RegisterComponent,
-    ProfileComponent,
-    AboutComponent
+    UpcomingContestComponent
   ],
   imports: [
     BrowserModule,
