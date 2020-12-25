e4c38c0992d705abf6c710a5cfec4656c6a71402
Dhiraj Govindvira <dhirajgovindvira1112@gmail.com>
2020-12-25 20:14:18 +0530

    home right side code changes.

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild("code") codeEle: ElementRef;
   constructor() { }
+
   title: string = "</Decoder>";
-  openBracket = "{";
-  closeBracket = "}";
+  default_code: string = `
+<span class="text-danger">While</span>(alive)
+{
+    <span style="color: #33C9FF">eat</span>();
+    <span style="color: #33C9FF">code</span>();
+    <span style="color: #33C9FF">sleep</span>();
+    <span style="color: #33C9FF">repeat</span>();
+}
+`;
+
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.codeEle.nativeElement.innerHTML = this.default_code;
   }
 }
