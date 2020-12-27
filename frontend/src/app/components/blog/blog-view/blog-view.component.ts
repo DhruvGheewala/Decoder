9a703bf77ecfa5c0cee8973745f93741c9f540b5
Dhiraj Govindvira <dhirajgovindvira1112@gmail.com>
2020-12-27 14:12:53 +0530

    Revert "Revert "Merge branch 'main' into DhruvGheewala""
    This reverts commit b929ec119f78ab65d73b2e2f5754f0b42fa5327a.

---
+++ frontend/src/app/components/blog/blog-view/blog-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-view',
+  templateUrl: './blog-view.component.html',
+  styleUrls: ['./blog-view.component.css']
+})
+export class BlogViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
