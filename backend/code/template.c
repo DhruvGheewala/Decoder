43def2b46fc3e8a3643af86273d9ba1ed885c48e
DhruvGheewala <dhruvgheewala.4g@gmail.com>
2021-01-01 15:16:21 +0530

    updated CODE-IDE Component  ## changes   - updated gitignore file, removed temporary code files(specifically Decoder.*)   - updated TODO.md   - updated run.bat   - updated style.css   - updated user.service   - updated code-ide. html & ts

--- backend/code/template.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    // your code goes here ...
-    return 0;
-}
--- backend/code/template.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    // your code goes here ...
-    return 0;
-}
