2d5e1abfef53f86826d5be19a14455ca5822c440
kushalp13 <kushal.p13@yandex.com>
2021-01-15 16:26:02 +0530

    Updated angular version

--- frontend/src/app/components/blog/blog-edit/blog-edit.component.spec.ts
+++ frontend/src/app/components/blog/blog-edit/blog-edit.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BlogEditComponent } from './blog-edit.component';
 
@@ -6,7 +6,7 @@ describe('BlogEditComponent', () => {
   let component: BlogEditComponent;
   let fixture: ComponentFixture<BlogEditComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BlogEditComponent ]
     })
--- frontend/src/app/components/code/code-view/code-view.component.spec.ts
+++ frontend/src/app/components/code/code-view/code-view.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CodeViewComponent } from './code-view.component';
 
@@ -6,7 +6,7 @@ describe('CodeViewComponent', () => {
   let component: CodeViewComponent;
   let fixture: ComponentFixture<CodeViewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CodeViewComponent ]
     })
